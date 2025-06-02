45fcfe93835bdfeb14a16d3ccc39fe9d3e38ec31
pavlovitenko <139057349+pavlovitenko@users.noreply.github.com>
2025-06-02 11:42:46 +0100

    Improved header

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+  document.querySelectorAll('.header-nav-item').forEach(item => {
+    item.addEventListener('click', function (e) {
+      // Якщо клік був по <a> або вкладених посиланнях, нічого не робимо — браузер сам перейде
+      if (e.target.closest('a')) return;
+
+      // Отримаємо головне <a> цього пункту меню
+      const link = this.querySelector('a');
+      if (link && link.href) {
+        window.location.href = link.href;
+      }
+    });
+  });
+
 // counting
 function animateCounter(el, duration = 8000) {
   const target = +el.getAttribute("data-target");
